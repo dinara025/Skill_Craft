b102bae78d2e99255509d0b034ac7480f5cf0e6c
NuwaniSeram <nuwaniseram@gmail.com>
2025-04-21 12:08:28 +0530

    set up the spring boot project

---
+++ skill-craft/src/main/java/com/paf/skillcraft/skill_craft/SkillCraftApplication.java
@@ -0,0 +1,13 @@
+package com.paf.skillcraft.skill_craft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillCraftApplication.class, args);
+	}
+
+}
