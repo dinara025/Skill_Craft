7784e506740e9549ee3480f25d4e610d623306c0
Nimsi Dharmaratne <136379473+dinara025@users.noreply.github.com>
2025-04-25 16:39:04 +0530

    implement login/register-frontend

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_BASE = 'http://localhost:8080/api/user';
+
+export const registerUser = (username, password) =>
+  axios.post(`${API_BASE}/register`, null, {
+    params: { username, password }
+  });
+
+export const loginUser = (username, password) =>
+  axios.post(`${API_BASE}/login`, null, {
+    params: { username, password }
+  });
