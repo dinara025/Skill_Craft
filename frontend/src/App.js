231b4c71094110cae914a0d6ea338582daaf64cb
Chethaka Lakshitha <dewdslliya@gmail.com>
2025-05-06 19:45:19 +0530

    Updated the App.js
    Updated the App.js to have the admin login and send the user details correctly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import FollowSystem from './components/FollowSystem';
 import CreatePost from './components/CreatePost'; 
 import LearningPlans from './components/LearningPlans';
 import UpdatePost from './components/UpdatePost';
+import AdminLoginPage from './components/AdminLoginPage';
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState(null);
@@ -19,7 +20,7 @@ function App() {
   
     const fetchUserDetails = async (username, token) => {
       try {
-        const response = await fetch(`http://localhost:8080/api/auth/userDetails/${username}`, {
+        const response = await fetch(`http://localhost:8080/userDetails/${username}`, {
           headers: {
             Authorization: `Bearer ${token}`,
             Accept: 'application/json',
@@ -64,6 +65,8 @@ function App() {
   
       const userData = await response.json();
       setLoggedInUser({ ...userData, token });
+      console.log("the id of the user: ",userData.id);
+      localStorage.setItem('usersId', userData.id);
   
     } catch (err) {
       console.error('Login error:', err);
@@ -80,6 +83,7 @@ function App() {
           <Routes>
             <Route path="/" element={<AuthPage onLogin={handleLogin} />} />  {/* ✅ Changed to use handleLogin */}
             <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/admin-login" element={<AdminLoginPage/>}/>
           </Routes>
         ) : (
           <Routes>
@@ -87,7 +91,7 @@ function App() {
             <Route path="/" element={<MainPage user={loggedInUser} />} />
             <Route path="/create-post" element={<CreatePost user={loggedInUser.id} />} />
             <Route path="/follow-system" element={<FollowSystem senderId={loggedInUser.username} />} />
-            <Route path="/learning-plans" element={<LearningPlans userId={loggedInUser.username} />} /> {/* Passed username as ID */}
+            <Route path="/learning-plans" element={<LearningPlans user={loggedInUser} />} />
             <Route path="/update-post/:id" element={<UpdatePost user={loggedInUser} />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
